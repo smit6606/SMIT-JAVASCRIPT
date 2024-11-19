b8e9f7edf026a80c5e0c83f3664917a9ca66a60a
smit6606 <smeetgarala6606@gmail.com>
2024-11-19 19:07:54 +0530

    output 19 November 2024

---
+++ output.js
@@ -0,0 +1,32 @@
+/*/
+    1. console.log()
+    2. alert()
+    3. document.write()
+    4. DOM
+*/
+
+// 1. console.log()
+
+// console.log("Hello, Jay Shree Krishna")
+
+// console.warn("Don't Disturb")
+
+// console.error("shut up!")
+
+// console.log(3+5)
+
+// 2. alert()
+
+// alert("Good Morning")
+
+// 3. document.write()
+
+// document.write("JavaScript Lecture is Taken by Girish Sir")
+
+// 4. DOM
+
+// document.getElementById("java").innerHTML=" today we learn the output topic"
+
+
+
+
