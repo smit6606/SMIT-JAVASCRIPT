a77d7ea6f5ffa62f9fad4593b67f661460ea5b32
Smit Garala <smeetgarala6606@gmail.com>
2024-11-20 18:35:20 +0530

    Variable Declaration 20 November 2024

---
+++ variable.js
@@ -0,0 +1,40 @@
+/*/
+
+variable => variable is a like this container to store information or data
+
+*/
+
+// Initalization
+
+var a;
+let x;
+const PI= 3.14;    //assign
+
+a = 10;
+console.log(a);
+
+// Data types 
+
+// 1) Primitive
+//     - Number
+//     - String
+//     - Boolean
+//     - null
+//     - undefined
+//     - Symbol
+//     - BigInt
+
+// 2) Non-Primitive
+//     - Object
+
+let num = 10;
+console.log(num);
+console.log(typeof num);
+
+let num2 = "smit";
+console.log(num2);
+console.log(typeof num2);
+
+let num3 = true;
+console.log(num3);
+console.log(typeof num3);
