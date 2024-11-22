bd5646da31d27e78abf41c26b843ec73edb94c66
Smit Garala <smeetgarala6606@gmail.com>
2024-11-22 18:59:50 +0530

    Conditional 22 November 2024

---
+++ conditional.js
@@ -0,0 +1,102 @@
+/* Conditional Statement --> if else, nested, ladder, switch, ternary operator 
+
+1) if-else Statement
+
+if(condition){
+    statement...
+}
+else{
+    statement...
+}
+
+*/
+
+// find small no between two number by if-else statement
+
+// let num1 = 20, num2 = 50;
+
+// if(num1 < num2){
+//     console.log("Small number is:", num1)
+// }
+// else{
+//     console.log("Small number is:", num2)
+// }
+
+/* 
+
+2)Ternary Operator
+
+condition ? true : false
+
+*/
+
+// find small no between two number by ternary operator
+
+// num1 < num2 ? console.log("Small no is:", num1) : console.log("Small no is:", num2)
+
+// console.log("Small no is:", num1 < num2 ? num1 : num2)
+
+/*
+
+Swapping Two Number :-
+     i) Third Variable
+     ii) Without Third Variable
+     iii) XOR
+    
+*/
+
+    // i) Third Variable
+
+    // let a = 10, b = 20, c
+    // console.log("Before Swapping -------->");
+    // console.log("A ------->", a);
+    // console.log("B ------->", b);
+
+    // c = a;
+    // a = b;
+    // b = c;
+
+    // console.log("After Swapping -------->");
+    // console.log("A ------->", a);
+    // console.log("B ------->", b);
+
+    // ii) Without Third Variable
+
+    // let x = 10, y = 20;
+    // console.log("Before Swapping -------->");
+    // console.log("X ------->", x);
+    // console.log("Y ------->", y);
+
+    // x = x + y;
+    // y = x - y;
+    // x = x - y;
+
+    // console.log("After Swapping -------->");
+    // console.log("X ------->", x);
+    // console.log("Y ------->", y);
+
+    // iii) XOR 
+
+    // let x = 10, y = 20;
+    // console.log("Before Swapping -------->");
+    // console.log("X ------->", x);
+    // console.log("Y ------->", y);
+
+    // x = x ^ y;
+    // y = x ^ y;
+    // x = x ^ y;
+
+    // console.log("After Swapping -------->");
+    // console.log("X ------->", x);
+    // console.log("Y ------->", y);
+
+
+
+
+
+
+
+
+
+
+
