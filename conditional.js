9b0777160d479494b40e8d1616502bc6f82faebc
Smit Garala <smeetgarala6606@gmail.com>
2024-11-26 22:19:47 +0530

    Conditional - Nested & Ladder  26 November 2024

--- conditional.js
+++ conditional.js
@@ -45,50 +45,162 @@ Swapping Two Number :-
     
 */
 
-    // i) Third Variable
+// i) Third Variable
 
-    // let a = 10, b = 20, c
-    // console.log("Before Swapping -------->");
-    // console.log("A ------->", a);
-    // console.log("B ------->", b);
+// let a = 10, b = 20, c
+// console.log("Before Swapping");
+// console.log("A ------->", a);
+// console.log("B ------->", b);
 
-    // c = a;
-    // a = b;
-    // b = c;
+// c = a;
+// a = b;
+// b = c;
 
-    // console.log("After Swapping -------->");
-    // console.log("A ------->", a);
-    // console.log("B ------->", b);
+// console.log("After Swapping");
+// console.log("A ------->", a);
+// console.log("B ------->", b);
 
-    // ii) Without Third Variable
+// ii) Without Third Variable
 
-    // let x = 10, y = 20;
-    // console.log("Before Swapping -------->");
-    // console.log("X ------->", x);
-    // console.log("Y ------->", y);
+// let x = 10, y = 20;
+// console.log("Before Swapping");
+// console.log("X ------->", x);
+// console.log("Y ------->", y);
 
-    // x = x + y;
-    // y = x - y;
-    // x = x - y;
+// x = x + y;
+// y = x - y;
+// x = x - y;
 
-    // console.log("After Swapping -------->");
-    // console.log("X ------->", x);
-    // console.log("Y ------->", y);
+// console.log("After Swapping");
+// console.log("X ------->", x);
+// console.log("Y ------->", y);
 
-    // iii) XOR 
+// iii) XOR 
 
-    // let x = 10, y = 20;
-    // console.log("Before Swapping -------->");
-    // console.log("X ------->", x);
-    // console.log("Y ------->", y);
+// let x = 10, y = 20;
+// console.log("Before Swapping");
+// console.log("X ------->", x);
+// console.log("Y ------->", y);
 
-    // x = x ^ y;
-    // y = x ^ y;
-    // x = x ^ y;
+// x = x ^ y;
+// y = x ^ y;
+// x = x ^ y;
 
-    // console.log("After Swapping -------->");
-    // console.log("X ------->", x);
-    // console.log("Y ------->", y);
+// console.log("After Swapping");
+// console.log("X ------->", x);
+// console.log("Y ------->", y);
+
+
+// 3) Nested if-else
+
+/*
+
+if (condition) {
+    if (condition) {
+        statement
+    }
+    else {
+        statement
+    }
+}
+else {
+    statement
+}
+
+*/
+
+// Example :- Three Variable => Smallest Value by nested if-else
+
+let a = 20, b = 0, c = 4;
+
+if (a < b) {
+    if (a < c) {
+        console.log("Small Number is :", a);
+    }
+    else {
+        console.log("Smaller Number is :", c);
+    }
+}
+else {
+    if (b < c) {
+        console.log("Smallester Number is :", b);
+    }
+    else {
+        console.log("Smallest Number is :", c);
+    }
+}
+
+// Example :- Three Variable => Greatest Value by nested if-else
+
+let x = 20, y = 0, z = 4;
+
+if (x > y) {
+    if (x > z) {
+        console.log("Greater Number is :", x);
+    }
+    else {
+        console.log("Greatest Number is :", z);
+    }
+}
+else {
+    if (y > z) {
+        console.log("Great Number is :", y);
+    }
+    else {
+        console.log("Greatester Number is :", z);
+    }
+}
+
+
+// 4) Ladder if-else :-
+
+/*
+
+if (condition) {
+    statement
+}
+else if (condition) {
+    statement
+}
+else {
+    statement
+}
+
+*/
+
+// Example :- Three Variable => Smallest Value by ladder if-else
+
+let p = 300, q = 40, r = 20;
+
+if (p < q && p < r) {
+    console.log("Smallest no is :", p);
+}
+else if (q < r) {
+    console.log("Smallest no is :", q);
+}
+else {
+    console.log("Smallest no is :", r);
+}
+
+// Example :- Three Variable => Greatest Value by ladder if-else
+
+let i = 300, j = 40, k = 20;
+
+if (i > j && i > k) {
+    console.log("Greatest no is :", i);
+}
+else if (j > k) {
+    console.log("Greatest no is :", j);
+}
+else {
+    console.log("Greatest no is :", k);
+}
+
+
+
+
+
+
 
 
 
