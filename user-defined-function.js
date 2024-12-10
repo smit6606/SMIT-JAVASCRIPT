cc896a818ef054566843b2fb15de9f0abb4a3582
Smit Garala <smeetgarala6606@gmail.com>
2024-12-10 16:12:28 +0530

    User Defined Function   09 December 2024

---
+++ user-defined-function.js
@@ -0,0 +1,39 @@
+/*
+    1. No Argument No Return
+    2. No Argument With Return
+    3. With Argument No Return
+    4. With Argument With Return
+*/
+
+// 1. No Argument No Return.
+
+// function sayGreeting(){
+//     console.log("Good Morning...!");
+// }
+// sayGreeting();
+
+// 2. No Argument With Return.
+
+// function greeting(){
+//     return("JavaScript...!");
+// }
+// greeting();
+
+// 3. With Argument No Return.
+
+// function addition(a,b){
+//     console.log("Addition is :",a+b);
+// }
+// addition(10,20);
+
+// 4. With Argument With Return.
+
+// function voting(age){
+//     if(age >= 18){
+//         console.log("Are You Eligible For Voting.");
+//     }
+//     else{
+//         console.log("Are You Not Eligible For Voting.");
+//     }
+// }
+// console.log(voting(21));
