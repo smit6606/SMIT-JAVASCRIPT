1dbfee2936cd4be9dfedf46646f805419cc86d89
Smit Garala <smeetgarala6606@gmail.com>
2024-12-11 19:03:46 +0530

    Function As Variable, IIFE , Clouser   11 December 2024

---
+++ function-as-variable.js
@@ -0,0 +1,40 @@
+// Function as a Variable
+
+// let Product = function (x, y) {
+//     console.log(x * y);
+// }
+
+// Product(10, 20)
+
+
+// Arrow Function 
+
+// const Sum = (x, y) => {
+//     return x + y;
+// }
+
+// const Sub = () => "HELLO JAVASCRIPT"
+
+// console.log(Sub())
+// console.log(Sum(3, 5))
+
+
+// IIFE (Immediately Invoking Function Expression)
+
+// (function (m, n) {
+//      console.log("IIFE called", m / n);
+// })(9, 3);
+
+// // Clouser Function / Nested Function 
+
+// function outerFun(){
+//     function innerFun(){
+//         return "Inner Function"
+//     }
+//     console.log("outer Function Called");
+//     return(innerFun);
+// }
+
+// let c = outerFun()
+// console.log(c())
+
