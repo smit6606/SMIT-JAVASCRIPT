8220756057450a4f5f46f3fdd025d7604c4e00c2
Smit Garala <smeetgarala6606@gmail.com>
2025-01-01 19:00:40 +0530

    Constructor  01 January 2025

---
+++ constructor.js
@@ -0,0 +1,51 @@
+/*
+Constructor => Special method of class (automatically call when object ic created)
+
+There Are Two Types of Methods
+i) Default Constructor
+ii) Parameterized Constructor
+*/
+
+// i) Default Constructor
+
+class Test {
+    constructor() {
+        console.log("Default Constrctor called......")
+    }
+    sayHello() {
+        console.log("Hello! Good Morning. Jay Shree Krishna")
+    }
+    sumAddition() {
+        console.log("Sum of A + B:", a + b);
+
+    }
+}
+let smit = new Test()
+smit.sayHello()
+
+// ii) Parameterized Constructor
+
+class Demo {
+    constructor(name, a, b) {
+        this.name = name;
+       
+        this.a = a;
+        this.b = b;
+
+        this.sum = a + b;
+        console.log("Constructor called......");
+        console.log("Sum of a + b in constructor:", this.sum);
+    }
+
+    sayHello() {
+        console.log("Hello! Good Morning. Jay Shree Krishna", this.name);
+    }
+
+    sumAddition() {
+        console.log("Sum of A + B:", this.sum);
+    }
+}
+
+let jay = new Demo( "Smit", 12, 56);
+// jay.sayHello();
+jay.sumAddition();
